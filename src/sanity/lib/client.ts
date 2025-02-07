4bf152df89907d5b92a90aea8c07598be6ac6883
faizamairaj <faizamairaj0@gmail.com>
2025-02-07 13:44:12 +0500

    market-place-hackathon

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -6,6 +6,6 @@ export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   apiVersion,
-  useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
+  useCdn:false, // Set to false if statically generating pages, using ISR or tag-based revalidation
 token: process.env.SANITY_API_TOKEN,
 })
